b28e0bc3270f355093f1eb1e9b0698721aa7c02e
NikolajDev <nikolajkalinin2006@gmail.com>
2026-03-18 21:35:25 +0100

    adding a first messages (Chirps)

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Chirp;
 
 class ChirpController extends Controller
 {
@@ -11,7 +12,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
+        $chirps = Chirp::with('user')->latest()->take(50)->get();
+
+        return view('home', ['chirps' => $chirps]);
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,19 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        @foreach ( $chirps as $chirp )
+        @forelse ($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
-                    <h2 class="text-xl font-bold">{{ $chirp['user'] }}</h2>
-                    <p class="mt-2">{{ $chirp['message'] }}</p>
+                    <div>
+                        <div class="font-semibold"> {{ $chirp->user  ? $chirp->user->name : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $chirp->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $chirp->created_at->diffForHumans() }}
+                        </div>
+                    </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-center text-gray-500 mt-8">No chirps yet. Be the first to chirp!</p>
+        @endforelse
 </x-layout>
