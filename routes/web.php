ff85e15c85fa14d987ca4d493342e6c805316e20
NikolajDev <nikolajkalinin2006@gmail.com>
2026-03-19 19:24:49 +0100

    Final deployment of Working Sign in and SIgn Up page, with the databse in the background, also learning to use the DB in the Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
 
 Route::get('/', [ChirpController::class, 'index']);
 
@@ -18,4 +19,10 @@ Route::middleware('auth')->group(function () {
 
 Route::view('/register', 'auth.register')->middleware('guest')->name('register');
 Route::post('/register', Register::class)->middleware('guest');
-Route::post('/logout', \App\Http\Controllers\Auth\Logout::class)->middleware('auth')->name('logout');
+
+
+Route::post('/logout', Logout::class)->middleware('auth')->name('logout');
+
+
+Route::view('/login', 'auth.login')->middleware('guest')->name('login');
+Route::post('/login', Login::class)->middleware('guest');
